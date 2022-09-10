7635b902ba97e3de9da1cd3392ff5f174bafe70a
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-09-10 22:51:34 +0600

    Fetch data from django api.
    Fetch data from django api.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
-
-// Bootstrap library 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Components import 
-import Navigation from './components/Navigation/Navigation';
+import "./App.css";
+import BlogPost from "./components/BlogPost/BlogPost";
+// Components import
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navigation />
+            <hr />
+            <BlogPost />
+        </div>
+    );
 }
 
 export default App;
