d0a341ba28657b732c5ce155953cee8ea1a8b88b
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-12-14 00:41:37 +0600

    sitebar update
    sitebar update

--- src/components/Home/Sidebar.js
+++ src/components/Home/Sidebar.js
@@ -2,25 +2,27 @@ import React from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 // import Sonnet from "../../components/Sonnet";
+import { IoChevronForwardOutline } from "react-icons/io5";
 
 import TextTruncate from "react-text-truncate";
 import Post from "../BlogPost/Post";
 import { Link } from "react-router-dom";
 
 const Sidebar = (props) => {
-    const  post = props.post;
+    const post = props.post;
 
     return (
         <div className="Sidebar bg-white">
             <div className="Sidebar-body">
                 <Tabs defaultActiveKey="hot-blog" id="sidebar-title" className="mb-3" justify>
                     <Tab eventKey="new-blog" title="নতুন ব্লগ">
-                        <ul className="ps-0">
+                        <ul className="mb-2 ps-0">
                             {post && post.map((Item, index) => {
                                 const { id, title, slug, description, image, category } = Item;
                                 return (
                                     <li key={id}>
-                                        <Link to={`blog-details/${slug}`} state={{ id, title, description, image }}>
+                                        <Link className="d-flex  align-items-center" to={`blog-details/${slug}`} state={{ id, title, description, image }}>
+                                            <IoChevronForwardOutline />
                                             <TextTruncate line={1} truncateText="...." text={title} />
                                         </Link>
                                         <hr className="m-2 ms-0" />
@@ -30,13 +32,14 @@ const Sidebar = (props) => {
                         </ul>
                     </Tab>
                     <Tab eventKey="hot-blog" title="হট ব্লগ">
-                        <ul className="ps-0">
+                        <ul className="mb-2 ps-0">
                             {post && post.map((Item, index) => {
                                 const { id, title, slug, description, image, category } = Item;
                                 return (
                                     <li key={id}>
-                                        <Link to={`blog-details/${slug}`} state={{ id, title, description, image }}>
-                                            <TextTruncate line={1} truncateText="...." text={title} />
+                                        <Link className="d-flex  align-items-center" to={`blog-details/${slug}`} state={{ id, title, description, image }}>
+                                            <IoChevronForwardOutline />
+                                            <TextTruncate line={1} truncateText="..." text={title} />
                                         </Link>
                                         <hr className="m-2 ms-0" />
                                     </li>
@@ -45,12 +48,13 @@ const Sidebar = (props) => {
                         </ul>
                     </Tab>
                     <Tab eventKey="others" title="অন্যান্য">
-                        <ul className="ps-0">
+                        <ul className="mb-2 ps-0">
                             {post && post.map((Item, index) => {
                                 const { id, title, slug, description, image, category } = Item;
                                 return (
                                     <li key={id}>
-                                        <Link to={`blog-details/${slug}`} state={{ id, title, description, image }}>
+                                        <Link className="d-flex align-items-center" to={`blog-details/${slug}`} state={{ id, title, description, image }}>
+                                            <IoChevronForwardOutline />
                                             <TextTruncate line={1} truncateText="...." text={title} />
                                         </Link>
                                         <hr className="m-2 ms-0" />
