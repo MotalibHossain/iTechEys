d61653f8129aff3c643e5a2c98ddc9986c04754b
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2023-02-15 19:33:36 +0600

    ui update.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Post from "../components/BlogPost/Post";
 import { Link } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
 // icon
 import { MdOutlineDoubleArrow, MdOutlineSearch } from "react-icons/md";
 import { IoChevronForwardOutline } from "react-icons/io5";
@@ -35,7 +36,26 @@ const Home = () => {
             <section className="Section-1 mt-3">
                 <div className="row">
                     <div className="col-lg-8 col-md-12 col-sm-12">
-                        <div className="main-banner">
+                        <div className="home-slider">
+                            <Carousel>
+                                {post &&
+                                    post.map((Item, index) => {
+                                        const { id, title, slug, description, image, category } = Item;
+                                        return (
+                                            <Carousel.Item>
+                                                <div className="banner-img-overla">
+                                                    <img className="d-block w-100" src={image} alt="First slide" />
+                                                </div>
+                                                <Carousel.Caption>
+                                                    <h3>{title}</h3>
+                                                    <p>{description}</p>
+                                                </Carousel.Caption>
+                                            </Carousel.Item>
+                                        );
+                                    })}
+                            </Carousel>
+                        </div>
+                        {/* <div className="main-banner">
                             <div className="banner-img">
                                 <div className="banner-img-overlay">
                                     <img src={latestPost.image} alt="image" />
@@ -53,7 +73,7 @@ const Home = () => {
                                     {latestPost.description}
                                 </span>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                     <div className="col-lg-4 col-md-4 col-sm-4 pl-0 side-title">
                         <ul className="ps-0">
@@ -105,13 +125,13 @@ const Home = () => {
 
             <section className="Section-3">
                 <div className="row mb-30">
-                    <div className="col-lg-9 col-md-9 Card-container">
+                    <div className="col-lg-8 col-md-8 Card-container">
                         <div className="Card-body">
                             <div className="row">
                                 {post.map((Item, index) => {
                                     const { id, title, slug, description, image, author, category } = Item;
                                     return (
-                                        <div className="col-lg-4 col-md-6 col-sm-6" key={index}>
+                                        <div className="col-lg-6 col-md-6 col-sm-6" key={index}>
                                             <div className="post-card pb-0">
                                                 <div className="post-card-img">
                                                     <img src={image} alt="post image" />
@@ -123,7 +143,9 @@ const Home = () => {
                                                                 src="https://bootdey.com/img/Content/avatar/avatar7.png"
                                                                 alt="Profile Image"
                                                             />
-                                                            <div className="d-flex mt-2 text-capitalize">{author.username}</div>
+                                                            <div className="d-flex mt-2 text-capitalize">
+                                                                {author.username}
+                                                            </div>
                                                         </div>
                                                         <p className="time mb-0 mt-2">{category.name}</p>
                                                     </div>
@@ -148,7 +170,7 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-3 col-md-3 blog-aside">
+                    <div className="col-lg-4 col-md-4 blog-aside">
                         <Sidebar post={post} />
                     </div>
                 </div>
