859f8f85b26b3a63844ce19dd47c74662989a34d
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2023-05-06 21:47:22 +0600

    project routing update.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes, Redirect, Navigate } from "react-router-dom";
 import "./App.css";
 import "./style/Style.css";
@@ -8,8 +8,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 // Components import
-import Header from "./Layout/Header";
-import Footer from "./Layout/Footer.js";
+import Layout from "./Layout/Layout";
 
 // pages
 import Dashboard from "./Pages/Dashboard";
@@ -19,40 +18,51 @@ import Contact from "./Pages/Contact";
 import SubmitPost from "./Pages/SubmitPost";
 import BlogDetails from "./Pages/BlogDetails";
 import UserProfile from "./Pages/UserProfile";
-import AuthorProfile  from "./Pages/AuthorProfile";
+import AuthorProfile from "./Pages/AuthorProfile";
+
 // user system import
 import Login from "./Pages/Login";
 import Registration from "./Pages/Registration";
 import Error from "./Pages/Error";
+
 // import redux material
-import { useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 
 function App() {
-    const {IsAuthenticate}=useSelector((state)=>state)
-    console.log("IsAuthenticate----",IsAuthenticate)
+    const { IsAuthenticate } = useSelector((state) => state);
+    console.log("IsAuthenticate----", IsAuthenticate);
     return (
         <>
             <BrowserRouter>
-                <Header />
                 <Routes>
-                    <Route path="/Dashboard" element={<Dashboard />}></Route>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="/service" element={<Service />}></Route>
-                    <Route path="/contact" element={<Contact />}></Route>
-                    <Route path="/save-post" element={<SubmitPost />}></Route>
-                    {/* Authenticate system  */}
-                    {/* <Route path="/login" element={IsAuthenticate ? <UserProfile /> : <Login />}></Route> */}
-                    <Route path="/login" element={IsAuthenticate ? <Navigate from="/login" to="/user-profile" /> : <Login />}></Route>
-                    <Route path="/registration" element={IsAuthenticate ? <Navigate from="/login" to="/user-profile" /> : <Registration />}></Route>
-                    {/* User system  */}
-                    <Route path="/user-profile" element={IsAuthenticate ? <UserProfile /> : <Navigate from="/user-profile" to="/login" />}></Route>
-                    <Route path="/author/:authname" element={<AuthorProfile />}></Route>
-                    {/* Subpage route  */}
-                    <Route path="/blog-details/:slug" element={<BlogDetails />}></Route>
-                    {/* Error page  */}
-                    <Route path="*" element={<Error />}></Route>
+                    <Route path="/Dashboard" element={<Dashboard />} />
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/service" element={<Service />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/save-post" element={<SubmitPost />} />
+                        {/* Authenticate system  */}
+                        {/* <Route path="/login" element={IsAuthenticate ? <UserProfile /> : <Login />}/> */}
+                        <Route
+                            path="/login"
+                            element={IsAuthenticate ? <Navigate from="/login" to="/user-profile" /> : <Login />}
+                        />
+                        <Route
+                            path="/registration"
+                            element={IsAuthenticate ? <Navigate from="/login" to="/user-profile" /> : <Registration />}
+                        />
+                        {/* User system  */}
+                        <Route
+                            path="/user-profile"
+                            element={IsAuthenticate ? <UserProfile /> : <Navigate from="/user-profile" to="/login" />}
+                        />
+                        <Route path="/author/:authname" element={<AuthorProfile />} />
+                        {/* Subpage route  */}
+                        <Route path="/blog-details/:slug" element={<BlogDetails />} />
+                        {/* Error page  */}
+                        <Route path="*" element={<Error />} />
+                    </Route>
                 </Routes>
-                <Footer />
             </BrowserRouter>
             <ToastContainer />
         </>
