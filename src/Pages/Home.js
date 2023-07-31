972b17d6fc2664a8e6faa395dd9f5fbe0689fd53
MotalibHossain <motalibhossainrbk33@gmail.com>
2023-08-01 00:16:55 +0600

    update.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -29,12 +29,12 @@ const Home = () => {
         });
     }, []);
 
-    // Load more functionality 
-    const [loadIndex, setloadendIndex]=useState(4)
+    // Load more functionality
+    const [loadIndex, setloadendIndex] = useState(4);
     function calculateSliceArgs(item) {
         const endIndex = loadIndex + 2;
-        console.log(endIndex, item)
-        setloadendIndex(endIndex)
+        console.log(endIndex, item);
+        setloadendIndex(endIndex);
     }
 
     return (
@@ -139,8 +139,12 @@ const Home = () => {
                                     <PostCard item={Item} key={index} />
                                 ))}
                                 <div className="col-lg-12 mb-3 text-center">
-                                    <button className="btn-animate" onClick={()=>calculateSliceArgs(2)} disabled={post.length === loadIndex ? 'true':''}>
-                                       <span>Load More</span>
+                                    <button
+                                        className="btn-animate"
+                                        onClick={() => calculateSliceArgs(2)}
+                                        disabled={post.length === loadIndex ? "true" : ""}
+                                    >
+                                        <span>Load More</span>
                                     </button>
                                 </div>
                             </div>
@@ -166,12 +170,12 @@ const Home = () => {
                                         index === 0
                                             ? "big"
                                             : "" || index === 1
-                                                ? "vertical"
-                                                : "" || index === 3
-                                                    ? "vertical"
-                                                    : "" || index === 5
-                                                        ? "horizontal"
-                                                        : "";
+                                            ? "vertical"
+                                            : "" || index === 3
+                                            ? "vertical"
+                                            : "" || index === 5
+                                            ? "horizontal"
+                                            : "";
 
                                     return (
                                         <div className={`gallery-item ${gridClass}`}>
@@ -180,11 +184,7 @@ const Home = () => {
                                             </div>
                                             <div className="overlay">
                                                 <Link to={`blog-details/${slug}`}>
-                                                    <TextTruncate
-                                                        line={2}
-                                                        element="p"
-                                                        text={title}
-                                                    />
+                                                    <TextTruncate line={2} element="p" text={title} />
                                                 </Link>
                                                 <div className="post-card-meta pb-1">
                                                     <Link to={`/author-profile/${author.username}`}>
@@ -210,8 +210,91 @@ const Home = () => {
                                 })}
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-4">
+                    <div className="col-lg-4 col-md-4"></div>
+                </div>
+            </section>
 
+            <section class="pt-5 pb-5">
+                <div class="row">
+                    <div class="col-6">
+                        <h3 class="mb-3">Carousel cards title </h3>
+                    </div>
+                    <div class="col-6 text-right">
+                        <a
+                            class="btn btn-primary mb-3 mr-1"
+                            href="#carouselExampleIndicators2"
+                            role="button"
+                            data-slide="prev"
+                        >
+                            <i class="fa fa-arrow-left"></i>
+                        </a>
+                        <a
+                            class="btn btn-primary mb-3 "
+                            href="#carouselExampleIndicators2"
+                            role="button"
+                            data-slide="next"
+                        >
+                            <i class="fa fa-arrow-right"></i>
+                        </a>
+                    </div>
+                    <div class="col-12">
+                        <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="row">
+                                        <div class="col-md-4 mb-3">
+                                            <div class="card">
+                                                <img
+                                                    class="img-fluid"
+                                                    alt="100%x280"
+                                                    src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d"
+                                                />
+                                                <div class="card-body">
+                                                    <h4 class="card-title">Special title treatment</h4>
+                                                    <p class="card-text">
+                                                        With supporting text below as a natural lead-in to additional
+                                                        content.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <div class="card">
+                                                <img
+                                                    class="img-fluid"
+                                                    alt="100%x280"
+                                                    src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698"
+                                                />
+                                                <div class="card-body">
+                                                    <h4 class="card-title">Special title treatment</h4>
+                                                    <p class="card-text">
+                                                        With supporting text below as a natural lead-in to additional
+                                                        content.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <div class="card">
+                                                <img
+                                                    class="img-fluid"
+                                                    alt="100%x280"
+                                                    src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a"
+                                                />
+                                                <div class="card-body">
+                                                    <h4 class="card-title">Special title treatment</h4>
+                                                    <p class="card-text">
+                                                        With supporting text below as a natural lead-in to additional
+                                                        content.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
